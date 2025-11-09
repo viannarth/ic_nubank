32ffd8b0deb8072aeac3f4555ceb3c0a0086bb3c
Arthur Vianna <viannarth@gmail.com>
2025-11-09 17:19:44 -0300

    refactor and add type hints to config constants

--- baseline/config/config.py
+++ baseline/config/config.py
@@ -1,11 +1,11 @@
 import yaml
 
-file_path = "./baseline/config/config.yaml"
+file_path = "./config.yaml"
 
 with open(file_path, "r", encoding='utf-8') as f:
     config_data = yaml.safe_load(f)
 
-EXAMS = config_data['exams']
-MODEL_NAMES = config_data['model_names']
-MODELS = config_data['models']
-IGNORED_QUESTIONS = config_data['ignored_questions']
+EXAMS: dict[str, list[str] | int] = config_data['exams']
+MODEL_NAMES: list[str] = config_data['model_names']
+MODELS: dict[str, list[str] | dict[str, str | int]] = config_data['models']
+IGNORED_QUESTIONS: dict[str, dict[str, list[str]]] = config_data['ignored_questions']
