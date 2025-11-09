05ddb05e675bd60eba23b25732daaa50ca853e24
Arthur Vianna <viannarth@gmail.com>
2025-11-09 19:42:21 -0300

    implement the creation of the model performances JSON file

--- baseline/model_evaluation.py
+++ baseline/model_evaluation.py
@@ -1,26 +1,6 @@
+from baseline.config import IGNORED_QUESTIONS
 import csv
 import json
-
-IGNORED_QUESTIONS = {
-    "cpa-10": {
-        "01": ["24", "25", "41"],
-        "02": ["28", "39", "41"],
-        "03": [],
-        "04": [],
-        "05": ["30", "34"],
-        "06": ["31"],
-        "07": [],
-        "08": ["24", "25"]
-    },
-    "ancord-aai": {
-        "01": ["43"],
-        "02": [],
-        "03": ["39"],
-        "04": ["43", "47", "49", "67"],
-        "05": ["78"],
-        "06": ["78"]
-    }
-}
 
 def get_answers(exam: str, test_number: str, filename: str) -> dict[str, str]:
 
@@ -51,8 +31,8 @@ def get_answers(exam: str, test_number: str, filename: str) -> dict[str, str]:
 
 def count_correct_answers(exam: str, test_number: str, model: str) -> int:
 
-    answers_path = "./dataset/answers/" + exam + "/test_" + test_number + "_answers.csv"
-    model_answers_path = "./models_answers/" + exam + "/" + model + "/test_" + test_number + "_answers.json"
+    answers_path = "./data/dataset/" + exam + "/answers/test_" + test_number + "_answers.csv"
+    model_answers_path = "./model_answers/" + exam + "/" + model + "/test_" + test_number + "_answers.json"
 
     test_answers = get_answers(exam, test_number, answers_path)
     model_answers = get_answers(exam, test_number, model_answers_path)
