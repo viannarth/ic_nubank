17fce8ff0786e31379984a230fc1c4d3cc249a0e
Arthur Vianna <viannarth@gmail.com>
2026-08-05 20:47:06 -0300

    feat: implement documents indexing and store index

---
+++ scripts/index_document.py
@@ -0,0 +1,52 @@
+import regex
+from dotenv import load_dotenv
+from baseline.config import MATERIAL_TOPICS
+from llama_index.core import Document, VectorStoreIndex
+from pathlib import Path
+import re
+
+def clean_markdown(exam:str, chapter:int) -> str:
+
+    md_path = "./data/material/" + exam + "/cap" + str(chapter) + ".md"
+    text = Path(md_path).read_text(encoding="utf-8")
+
+    # Remove author logo
+    text = re.sub(r"(?i)Logo\s*(?:da\s*)?Rafael\s*Tor[o]?\s*Academia\s*de\s*Finanças(?:\s*logo)?", "", text)
+    text = re.sub(r"(?i)Logo\s*(?:da\s*)?Rafael\s*Tor[o]", "", text)
+    text = re.sub(r"(?i)Rafael\s*Tor[o]?\s*Academia\s*de\s*Finanças(?:\s*logo)?", "", text)
+
+    # Remove footer
+    footer_pattern = r"(?i)Apostila\s*2025\s*\d*\s*CPA.*10\s*.*\s*Certificação\s*Profissional\s*ANBIMA\s*Série\s*10\s*\d*"
+    text = re.sub(footer_pattern, "", text)
+    
+    text = re.sub(r"\n\s*\d+\s*\n", "\n", text)
+    text = re.sub(r"\n{3,}", "\n\n", text)
+    text = re.sub(r"\u27A2", "", text)
+
+    return text.strip()
+
+def main() -> None:
+
+    # Retrieve OPENAI_API_KEY
+    load_dotenv(dotenv_path="./config.env")
+
+    # Toogle the exam
+    exam = "cpa-10"
+    # exam = "ancord-aai"
+
+    documents = []
+
+    for chapter, topic in MATERIAL_TOPICS[exam].items():
+        document_text = clean_markdown(exam, chapter)
+        document = Document(
+            text=document_text, 
+            metadata={"topic": topic}
+        )
+
+        documents.append(document)
+
+    index = VectorStoreIndex.from_documents(documents, show_progress=True)
+    index.storage_context.persist(persist_dir="./index")
+
+if __name__ == "__main__":
+    main()
